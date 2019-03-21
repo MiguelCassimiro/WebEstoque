2c0a2088a8f77d6fcfa53fa5771faa8bfdf089eb
MiguelCassimiro <mikassy99@gmail.com>
2019-03-20 22:09:04 -0300

    alterações 20/03

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -15,10 +15,10 @@ class ProductsTableSeeder extends Seeder
         for ($i=1; $i <=20; $i++) {
             $faker = Faker\Factory::create();
 
-            DB::table('providers')->insert(
+            DB::table('products')->insert(
             [
                 'descricao' => $faker->text(100),
-                'qtd' => $faker-randomNumber(),
+                'qtd' => $faker->randomNumber(),
                 'prc_venda' => $faker->randomFloat(2,100,200),
                 'prc_compra' => $faker->randomFloat(2,100,200),
                 'created_at' => \Carbon\Carbon::now()
