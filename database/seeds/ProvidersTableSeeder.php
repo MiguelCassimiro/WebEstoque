b50ec1b3ff42404d44622382154e39e98f580d88
MiguelCassimiro <mikassy99@gmail.com>
2019-03-27 21:55:05 -0300

    changes maden in 27th, mar

--- database/seeds/ProvidersTableSeeder.php
+++ database/seeds/ProvidersTableSeeder.php
@@ -12,5 +12,13 @@ class ProvidersTableSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker\Factory::create();
+
+        for($i=1;$i<=20;$i++){
+            DB::table('providers')->insert([
+                'nome' => $faker ->company(70),
+                'created_at' => \Carbon\Carbon::now()
+            ]);
+        }
     }
 }
